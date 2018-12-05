e291909c0f7ff0292ffb5a1e279aa54bbc48bc85
Pierre <pierrebullot.odnohikatsu@gmail.com>
2018-12-05 09:37:52 +0100

    first save

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Activité 4</title>
+		<link rel="stylesheet" href="./css/styles.css" />
+		<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+	</head>
+
+	<body>
+		<header>
+			</br>
+			<ul>
+				<span class="far fa-paper-plane">escape</span>
+
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Services</a></li>
+				<li><a href="#">Clients</a></li>
+				<li><a href="#">Team</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+			</br></br>
+			<p>Can you build the website of my dreams?</p>
+			<h1>Yup, we can do that.</h1>
+			</br>
+			<button>Learn More</button>
+			</br></br></br>
+		</header>
+		<div>	
+<!--
+			<img src="./images/night.png" alt="Photo by Ryan Heuer on Unsplash"/>
+-->
+		</div>
+		test
+	</body>
+    	<script src="js/scripts.js"></script>
+</html>	
